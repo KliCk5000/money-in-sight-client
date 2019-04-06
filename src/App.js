40e572309dfce0953c769b1a189e86e12b535b37
Nick Dean <klick5000@gmail.com>
2019-04-05 21:14:17 -0600

    setup travis and heroku deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            All your base are belong to us
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello World
           </a>
         </header>
       </div>
