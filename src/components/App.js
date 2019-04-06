bcab5e8bb5ff30862b53caad239a67ee08754408
Nick Dean <klick5000@gmail.com>
2019-04-05 23:12:56 -0600

    setup client file structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LandingPage from './LandingPage';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>App.js</h1>
+        <LandingPage />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  state = {  }
+  render() { 
+    return ( <p>Landing Page Component</p> );
+  }
+}
+ 
+export default LandingPage;
