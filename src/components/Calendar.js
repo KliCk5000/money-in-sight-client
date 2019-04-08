dcf01d6e03c9e311675d620b5572c43d5a2f6600
Nick Dean <klick5000@gmail.com>
2019-04-07 21:44:42 -0600

    setup calendar and added header

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
+import '../stylesheets/Calendar.css';
+
+import Days from './Days';
+
 class Calendar extends Component {
-  state = {};
+  state = {
+    month: 'April',
+  };
+
+  renderHeader() {
+    return <div>{this.state.month}</div>;
+  }
+
+  renderDaysOfWeek() {
+    const dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+    let dayJSX = dayNames.map((dayName) => {
+      return <div className="dayOfWeek" key={dayName}>{dayName}</div>;
+    });
+    console.log(dayJSX);
+    return <div>{dayJSX}</div>;
+  }
+
   render() {
     return (
-        <header role="banner">
-          <h1>Calendar View</h1>
-        </header>
+      <header role="banner">
+        <h1>Calendar View</h1>
+        <div className="calendar-container">
+          Here is my calendar
+          <div className="calendar">
+            {this.renderHeader()}
+            {this.renderDaysOfWeek()}
+          </div>
+        </div>
+      </header>
     );
   }
 }
---
+++ src/components/Days.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Days extends Component {
+  state = {  }
+  render() { 
+    return ( <div>Day</div> );
+  }
+}
+ 
+export default Days;
