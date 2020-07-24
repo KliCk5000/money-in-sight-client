2112c42cb391e5b470326b1414b8114c45cf684d
Nick Dean <klick5000@gmail.com>
2020-07-24 09:47:35 -0600

    changes to style

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Calendar from './Calendar';
-import Transactions from './Transactions';
-import AddTransactionForm from './AddTransactionForm';
+import Calendar from "./Calendar";
+import Transactions from "./Transactions";
+import AddTransactionForm from "./AddTransactionForm";
 
-import { addTransaction } from '../actions';
+import { addTransaction } from "../actions";
+
+import "../stylesheets/fullcalendar/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 class MainPage extends Component {
   state = {};
 
-  addTransactionSubmit = (values) => {
+  addTransactionSubmit = values => {
     let selectedDate = this.props.calendar.selectedDate;
     // We need to add the currently selected date to transaction
-    let completedTransaction = Object.assign({}, values, { date: selectedDate });
+    let completedTransaction = Object.assign({}, values, {
+      date: selectedDate
+    });
 
     this.props.addTransaction(completedTransaction);
   };
@@ -22,7 +32,20 @@ class MainPage extends Component {
     return (
       <div>
         <main role="main">
-          <Calendar className="calendar" />
+          {/* <Calendar className="calendar" /> */}
+          <FullCalendar
+            defaultView="dayGridMonth"
+            header={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+            }}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            ref={this.calendarComponentRef}
+            weekends={this.state.calendarWeekends}
+            events={this.state.calendarEvents}
+            dateClick={this.handleDateClick}
+          />
           <Transactions />
           <AddTransactionForm onSubmit={this.addTransactionSubmit} />
         </main>
@@ -32,11 +55,8 @@ class MainPage extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return state;
 };
 
-export default connect(
-  mapStateToProps,
-  { addTransaction },
-)(MainPage);
+export default connect(mapStateToProps, { addTransaction })(MainPage);
